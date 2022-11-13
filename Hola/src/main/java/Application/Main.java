5e80406b49061c2e76169e888679bb0a383a244a
Santiago2004tt <115126538+Santiago2004tt@users.noreply.github.com>
2022-11-12 21:57:37 -0500

    prueba

---
+++ Hola/src/main/java/Application/Main.java
@@ -0,0 +1,27 @@
+package Application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        URL url = new File("src/main/java/Views/HolaFx.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
